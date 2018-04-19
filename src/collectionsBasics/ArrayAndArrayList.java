47146e1dd7a7804c639a44e4eddf3370bd12aad6
victor olvera <victor olvera@TDG-MBL-GDL498.tiempodev.local>
2018-04-19 09:40:17 -0500

    Collections Basics

--- src/collectionsBasics/ArrayAndArrayList.java
+++ src/collectionsBasics/ArrayAndArrayList.java
@@ -1,4 +1,4 @@
-package collections;
+package collectionsBasics;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/collectionsBasics/DiamondOperator.java
+++ src/collectionsBasics/DiamondOperator.java
@@ -1,4 +1,4 @@
-package collections;
+package collectionsBasics;
 
 /**
  * @author victor olvera
